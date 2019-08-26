08ef3ded57bbbd35e07b0c6ea57f4c38e6636fd0
sacken0923 <sacken0923@gmail.com>
2019-08-26 15:54:36 +0900

    basic_certification

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -9,7 +9,8 @@ server '54.64.97.141', user: 'ec2-user', roles: %w{app db web}
 # server "db.example.com", user: "deploy", roles: %w{db}
 
 
-
+set :rails_env, "production"
+set :unicorn_rack_env, "production"
 # role-based syntax
 # ==================
 
