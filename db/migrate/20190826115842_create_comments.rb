eb03bf6fe2383deceeec5cd6ca259279c3d2300e
sacken0923 <sacken0923@gmail.com>
2019-08-27 16:08:29 +0900

    shusei

--- db/migrate/20190826115842_create_comments.rb
+++ db/migrate/20190826115842_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
       t.text  :comment,  null:false
-      # t.references :user,  null: false,  foreign_key: true
-      # t.references :item,  null: false,  foreign_key: true
+      t.references :user,  null: false,  foreign_key: true
+      t.references :item,  null: false,  foreign_key: true
       t.timestamps
     end
   end
