053cb948a59dc3fd47ab81a51451f9d7fecac445
4tk666 <vhfxhy5356@yahoo.co.jp>
2019-08-28 15:19:14 +0900

    formの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-　root to: "homes#index"
+  root to: "homes#index"
   resources :items, only: [:new, :create , :edit, :update]
-
-　resources :signup do
+  resources :signup do
     collection do
       get 'registration1'
       get 'registration2'
