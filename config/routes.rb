8dfda5ff4af5b900938094c52984d455d393de81
sacken0923 <sacken0923@gmail.com>
2019-09-05 15:44:43 +0900

    商品購入ページ_mk

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "homes#index"
-  resources :items, only: [:new, :create , :edit, :update]
+  resources :items, only: [:new, :create , :edit, :update] 
+  resources :purchases, only: [:new] 
 
   resources :signup do
     collection do
