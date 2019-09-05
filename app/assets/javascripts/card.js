55250caab6c3b1a1c1dbaf00970d9bd4c0790fb6
4tk666 <vhfxhy5356@yahoo.co.jp>
2019-09-05 16:47:00 +0900

    update

--- app/assets/javascripts/card.js
+++
@@ -1,9 +0,0 @@
-// $(function(){
-//   $(document).on('click','#token_submit',function(e){
-//     e.preventDefault();
-//     Payjp.setPublickey('pk_test_81e3463a3f7488b45e822c88')
-//     var card = {
-//     }
-//   });
-// });
-
