be3e81ba9b8c6ef08e3527cb098c306a5320b6bc
4tk666 <vhfxhy5356@yahoo.co.jp>
2019-09-07 15:19:14 +0900

    yaer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,28 +1,28 @@
 class User < ApplicationRecord
   # registration1
-  validates :nickname, presence: true,on: :validates_registration1
+  validates :nickname,                presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/
-  validates :email, presence: true, uniqueness: true,format: { with: VALID_EMAIL_REGEX },on: :validates_registration1
-  validates :password, presence: true,on: :validates_registration1
-  validates :password_confirmation, presence: true,on: :validates_registration1
-  validates :first_name, presence: true,on: :validates_registration1
-  validates :last_name, presence: true,on: :validates_registration1
-  validates :kana_first_name, presence: true,on: :validates_registration1
-  validates :kana_last_name, presence: true,on: :validates_registration1
-  validates :year, presence: true,on: :validates_registration1
-  validates :month, presence: true,on: :validates_registration1
-  validates :day, presence: true,on: :validates_registration1
+  validates :email,                   presence: true, uniqueness: true,format: { with: VALID_EMAIL_REGEX }
+  validates :password,                presence: true, length: { in: 7..128 } 
+  validates :password_confirmation,   presence: true, length: { in: 7..128 }
+  validates :first_name,              presence: true
+  validates :last_name,               presence: true
+  validates :kana_first_name,         presence: true
+  validates :kana_last_name,          presence: true
+  validates :year,                    presence: true
+  validates :month,                   presence: true
+  validates :day,                     presence: true
   # registration2
-  validates :tell, presence: true,on: :validates_registration2
+  validates :tell,                    presence: true
   # registration3
-  validates :address_last_name, presence: true,on: :validates_registration3
-  validates :address_first_name, presence: true,on: :validates_registration3
-  validates :kana_address_last_name, presence: true,on: :validates_registration3
-  validates :kana_address_first_name, presence: true,on: :validates_registration3
-  validates :prefecture, presence: true,on: :validates_registration3
-  validates :postal_code, presence: true,on: :validates_registration3
-  validates :city, presence: true,on: :validates_registration3
-  validates :address, presence: true,on: :validates_registration3
+  validates :address_last_name,       presence: true
+  validates :address_first_name,      presence: true
+  validates :kana_address_last_name,  presence: true
+  validates :kana_address_first_name, presence: true
+  validates :prefecture,              presence: true
+  validates :postal_code,             presence: true
+  validates :city,                    presence: true
+  validates :address,                 presence: true
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise  :database_authenticatable, :registerable,
@@ -30,3 +30,4 @@ class User < ApplicationRecord
   has_many :cards
   # accepts_nested_attributes_for :cards
 end
+# user = User.create(nickname: "aa")
