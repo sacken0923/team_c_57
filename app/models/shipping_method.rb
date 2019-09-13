687ceefc92d87a6db841a57721f82d2f7cb331c0
mamooooo74 <mamooo.74@icloud.com>
2019-09-13 13:30:47 +0900

    active_hash

---
+++ app/models/shipping_method.rb
@@ -0,0 +1,8 @@
+class Shipping_method < ActiveHash::Base
+  self.data = [
+      {id: 1, name: '未定'}, {id: 2, name: 'らくらくメルカリ便'}, {id: 3, name: 'ゆうメール'},
+      {id: 4, name: 'レターパック'},{id: 5, name: '普通郵便（定型、定型外)'},{id: 6, name: 'クロネコヤマト'},
+      {id: 7, name: 'ゆうパック'},{id: 8, name: 'クリックポスト'},{id: 9, name: 'ゆうパケット'}
+  ]
+end
+
