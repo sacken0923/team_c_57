80a98cf50f0da0a9f499f89aa4ee360e1746214c
mamooooo74 <mamooo.74@icloud.com>
2019-09-13 19:47:20 +0900

    修正

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,6 +1,10 @@
 class HomesController < ApplicationController
   def index
     @items = Item.all.limit(4).order(id: "DESC")
+    @ladys = Item.where(category_id: 1).limit(4).order(id: "DESC")
+    @mens = Item.where(category_id: 200).limit(4).order(id: "DESC")
+    @kids = Item.where(category_id: 346).limit(4).order(id: "DESC")
+    @kosemes = Item.where(category_id: 798).limit(4).order(id: "DESC")
   end
 
   def show
